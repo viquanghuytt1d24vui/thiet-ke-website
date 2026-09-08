4c3eeefa3bdb9ae8712cbf646a141c4f355f1c7a
viquanghuytt1d24vui <viquanghuys8plus@gmail.com>
2026-09-08 16:11:35 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,40 @@
 ﻿using System;
-
 namespace TinhToan
 {
     class Program
     {
+        static string xeploai(double diem)
+        {
+            if (diem < 0 || diem > 10)
+                return "nhap diem tu 0 toi 10";
+            else
+            {
+                string xep_loai = "";
+
+                if (diem >= 9.0)
+                    xep_loai = "xuat sac";
+                else if (diem >= 8.0)
+                    xep_loai = "Gioi";
+                else if (diem >= 6.5)
+                    xep_loai = "Kha";
+                else if (diem >= 5.0)
+                    xep_loai = "Trung binh";
+                else
+                    xep_loai = "Yeu";
+
+                return xep_loai;
+            }
+        }
+
         static void Main(string[] args)
         {
             try
             {
-                Console.Write("Nhap so nguyen thu nhat: ");
-                int a = int.Parse(Console.ReadLine()!);
-                Console.Write("Nhap so nguyen thu hai: ");
-                int b = int.Parse(Console.ReadLine()!);
-                Console.Write("Nhap so nguyen thu ba: ");
-                int a = int.Parse(Console.ReadLine()!);
-                Console.WriteLine("Tong cua 3 so : "+(a+b+c));
-                Console.WriteLine("Tich cua 3 so : "+(a*b*c));
-                double tb=(a+b+c)/3;
-                Console.WriteLine("Trung binh cong cua 3 so: "+tb);
+                Console.Write("Nhap diem: ");
+                double d = double.Parse(Console.ReadLine()!);
+
+                Console.WriteLine(xeploai(d));
+
                 Console.ReadKey();
             }
             catch (Exception ex)
